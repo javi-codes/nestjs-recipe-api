74b9965375d4d51082d49195924bc9c0187c8836
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2024-06-22 00:15:12 -0700

    Created DTo for creating a new recipe, finding one recipe too.

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -6,8 +6,11 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class RecipeService {
   constructor(private readonly prisma: PrismaService) {}
+
   create(createRecipeDto: CreateRecipeDto) {
-    return 'This action adds a new recipe';
+    return this.prisma.recipe.create({
+      data: createRecipeDto,
+    });
   }
 
   findAll() {
@@ -15,7 +18,9 @@ export class RecipeService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} recipe`;
+    return this.prisma.recipe.findUnique({
+      where: { id },
+    });
   }
 
   update(id: number, updateRecipeDto: UpdateRecipeDto) {
