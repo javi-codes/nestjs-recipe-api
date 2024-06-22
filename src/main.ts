3da6523887477e45af335745f9936392c1e49e3d
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2024-06-21 23:26:20 -0700

    Set up Swagger Document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Use DocumentBuilder to create a new Swagger document config.
+  const config = new DocumentBuilder()
+    .setTitle('Recipes API') // sets title for API
+    .setDescription('Recipes API') // sets the description of the API
+    .setVersion('1.0') //sets the version of the API
+    .build(); // Builds the document
+  // Create a swagger module with the  application instance and the document config.
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Setup Swagger module with the application instance and the Swagger document.
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
